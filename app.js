5c103da226b5194c285283e3a294334f92f9b530
Falk König <fk@falkkoenig.de>
2024-02-21 10:59:43 +0100

    listen of catch all

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 
-const hostname = "localhost";
-const port = 3000;
+// const hostname = "localhost";
+// const port = 3000;
 const app = express();
 
 app.get("/", (request, response) => {
   return response.json({ message: "Welcome to our note taking app" });
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+// app.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+app.get("*", (request, response) => {
+  response
+    .status(404)
+    .json({ success: false, message: "route not defined" });
 });
+
+module.exports = app;
