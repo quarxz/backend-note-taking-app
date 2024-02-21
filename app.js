688e1a04a360eafe61d42958d69e240ee5dcfd70
Falk König <fk@falkkoenig.de>
2024-02-21 10:15:51 +0100

    add-vercel.json

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const express = require("express");
 
+const hostname = "localhost";
+const port = 3000;
 const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Welcome to our note taking app" });
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
