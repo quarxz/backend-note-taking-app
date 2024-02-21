6a31f535f3380b48851a09007fe39b80616070eb
Falk König <fk@falkkoenig.de>
2024-02-21 15:03:28 +0100

    asda

--- app.js
+++ app.js
@@ -1,25 +1,65 @@
 const express = require("express");
-
-// const hostname = "localhost";
-// const port = 3000;
 const postgres = require("@vercel/postgres");
+const cors = require("cors");
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Welcome to our note taking app" });
-});
+app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-// app.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
-app.get("*", async (request, response) => {
+app.get("/", async (request, response) => {
   createNotes();
   // table was created => load data
   // wir bekommen hier ein Objekt mit rows zurück
   const { rows } = await postgres.sql`SELECT * FROM notes`;
-  response.status(404).json(rows);
-  // .json({ success: false, message: "route not defined" });
+  return response.json(rows);
+  //   return response.json({ message: "Welcome to our note taking app" });
+});
+
+app.get("/:id", async (request, response) => {
+  createNotes();
+  const { id } = request.params;
+  const { rows } =
+    await postgres.sql`SELECT * FROM notes WHERE id = ${id}`;
+
+  if (!rows.length) {
+    return response.json({ error: "Note not found." });
+  }
+
+  return response.json(rows[0]);
+});
+
+app.post("/", async (request, response) => {
+  createNotes();
+  const { content } = request.body;
+  if (content) {
+    await postgres.sql`INSERT INTO notes (content) VALUES (${content})`;
+    response.json({ message: "Successfully created note." });
+  } else {
+    response.json({ error: "Note NOT createt. Content is missing." });
+  }
+});
+
+// app.put("/:id", (request, response) => {
+//   createNotes();
+//   const { id } = request.params;
+//   const
+// });
+
+app.delete("/:id", async (request, response) => {
+  createNotes();
+  const { id } = request.params;
+  const { rowCount } =
+    await postgres.sql`DELETE FROM notes WHERE id = ${id}`;
+
+  if (!rowCount) {
+    return response.json({ error: "Note not found." });
+  }
+  response.json({ message: "Successfully delete note." });
+});
+
+app.get("*", (request, response) => {
+  response.status(404).json({ message: "route not defined" });
 });
 
 module.exports = app;
@@ -28,8 +68,12 @@ module.exports = app;
  * - we want to create a new table
  */
 async function createNotes() {
-  await postgres.sql`(CREATE TABLE IF NOT EXITS notes (
+  await postgres.sql`CREATE TABLE IF NOT EXISTS notes (
         id SERIAL PRIMARY KEY,
         content VARCHAR(255)
-    ))`;
+    )`;
 }
+
+// curl -X POST http://localhost:3000 -H 'Content-Type: application/json' -d '{"content":"Es geht der Bibabutzemann"}'
+
+// curl -X DELETE http://localhost:3000/6 -H 'Content-Type: plain/text' -d '{"content":"My second note"}'
