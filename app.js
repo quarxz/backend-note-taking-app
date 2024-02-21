5acfde71f77addc0c88a9216bd02128a2d335fcf
Falk König <fk@falkkoenig.de>
2024-02-21 11:41:34 +0100

    add get route for all notes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 // const hostname = "localhost";
 // const port = 3000;
+const postgres = require("@vercel/postgres");
 const app = express();
 
 app.get("/", (request, response) => {
@@ -12,10 +13,23 @@ app.get("/", (request, response) => {
 //   console.log(`Server running at http://${hostname}:${port}/`);
 // });
 
-app.get("*", (request, response) => {
-  response
-    .status(404)
-    .json({ success: false, message: "route not defined" });
+app.get("*", async (request, response) => {
+  createNotes();
+  // table was created => load data
+  // wir bekommen hier ein Objekt mit rows zurück
+  const { rows } = await postgres.sql`SELECT * FROM notes`;
+  response.status(404).json(rows);
+  // .json({ success: false, message: "route not defined" });
 });
 
 module.exports = app;
+
+/**
+ * - we want to create a new table
+ */
+async function createNotes() {
+  await postgres.sql`(CREATE TABLE IF NOT EXITS notes (
+        id SERIAL PRIMARY KEY,
+        content VARCHAR(255)
+    ))`;
+}
