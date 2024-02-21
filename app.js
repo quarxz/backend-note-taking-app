69c95934dcd3aa00e9ab80faf16164d774a3a66e
Falk König <fk@falkkoenig.de>
2024-02-21 15:05:00 +0100

    test

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (request, response) => {
   createNotes();
